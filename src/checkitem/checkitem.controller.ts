7466ad4dbe25ba1f6ed34ac8ae7a2464932aeff2
kjm9547 <woals8115@naver.com>
2025-12-10 00:13:13 +0900

    feat: 특정 기간 내 체크 리스트 조회 기능 추가

--- src/checkitem/checkitem.controller.ts
+++ src/checkitem/checkitem.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseGuards,
   Req,
+  Query,
 } from '@nestjs/common';
 import { CheckitemService } from './checkitem.service';
 import { CreateCheckitemDto } from './dto/create-checkitem.dto';
@@ -20,22 +21,39 @@ export class CheckitemController {
   @Post()
   create(@Req() req, @Body() createCheckitemDto: CreateCheckitemDto) {
     const userId = req.user.userId;
-    console.log('CheckitemController - create - userId:', userId);
     return this.checkitemService.create(createCheckitemDto, userId);
   }
 
+  // 특정 기간내 체크 리스트 조회 api
+  // 마지막 요일에서 며
+  // a~b 기간 사이의 모든 데이터 전달
+  // 유저 아이디와 기간을 통해 조회하고
+  // 시작일로부터 오늘을 계산해서 그시기
   @UseGuards(AuthGuard('jwt'))
   @Get()
-  findAll(@Req() req) {
+  findCheckListWithDuration(
+    @Req() req,
+    @Query('start') start: string,
+    @Query('end') end: string,
+  ) {
+    console.log('start, end', req, start, end);
     const userId = req.user.userId;
     return this.checkitemService.findAll(userId);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    console.log('???');
     return this.checkitemService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  findAll(@Req() req) {
+    console.log('???11111');
+    const userId = req.user.userId;
+    return this.checkitemService.findAll(userId);
+  }
   @Patch(':id')
   update(
     @Param('id') id: string,
